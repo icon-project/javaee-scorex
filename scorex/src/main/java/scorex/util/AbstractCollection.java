11b89469b0da40edf5c3c4380d7e54453a9026e0
Jaechang Namgoong <jnamgoong@iconloop.com>
2020-07-31 11:44:12 +0900

    Add the implementation of HashSet

--- scorex/src/main/java/scorex/util/AbstractCollection.java
+++ scorex/src/main/java/scorex/util/AbstractCollection.java
@@ -103,4 +103,20 @@ public abstract class AbstractCollection<E> implements Collection<E> {
         }
         return modified;
     }
+
+    public String toString() {
+        Iterator<E> i = iterator();
+        if (!i.hasNext())
+            return "[]";
+
+        StringBuilder sb = new StringBuilder();
+        sb.append('[');
+        for (;;) {
+            E e = i.next();
+            sb.append(e == this ? "(this Collection)" : e);
+            if (!i.hasNext())
+                return sb.append(']').toString();
+            sb.append(", ");
+        }
+    }
 }
---
+++ scorex/src/main/java/scorex/util/HashSet.java
@@ -0,0 +1,67 @@
+package scorex.util;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Set;
+
+/*
+ * An implementation of the Set interface backed by a HashMap instance.
+ */
+public class HashSet<E> extends AbstractCollection<E> implements Set<E> {
+
+    private final HashMap<E, Object> map;
+    private static final Object PRESENT = new Object();
+
+    public HashSet() {
+        map = new HashMap<>();
+    }
+
+    public HashSet(int initialCapacity) {
+        map = new HashMap<>(initialCapacity);
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return map.keySet().iterator();
+    }
+
+    @Override
+    public int size() {
+        return map.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return map.isEmpty();
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return map.containsKey(o);
+    }
+
+    @Override
+    public boolean add(E e) {
+        return map.put(e, PRESENT) == null;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        return map.remove(o) == PRESENT;
+    }
+
+    @Override
+    public void clear() {
+        map.clear();
+    }
+
+    @Override
+    public Object[] toArray() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public <T> T[] toArray(T[] a) {
+        throw new UnsupportedOperationException();
+    }
+}
