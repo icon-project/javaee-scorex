656d8470d003e7f9b225112909694de0df05731f
Jaechang Namgoong <jnamgoong@iconloop.com>
2020-07-31 11:24:23 +0900

    Refactor out AbstractCollection

---
+++ scorex/src/main/java/scorex/util/AbstractCollection.java
@@ -0,0 +1,106 @@
+package scorex.util;
+
+import java.util.Collection;
+import java.util.Iterator;
+
+public abstract class AbstractCollection<E> implements Collection<E> {
+
+    protected AbstractCollection() {
+    }
+
+    public abstract Iterator<E> iterator();
+
+    public abstract int size();
+
+    public boolean isEmpty() {
+        return size() == 0;
+    }
+
+    public boolean contains(Object o) {
+        Iterator<E> e = iterator();
+        if (o==null) {
+            while (e.hasNext())
+                if (e.next()==null)
+                    return true;
+        } else {
+            while (e.hasNext())
+                if (o.equals(e.next()))
+                    return true;
+        }
+        return false;
+    }
+
+    public Object[] toArray() {
+        throw new UnsupportedOperationException();
+    }
+
+    public <T> T[] toArray(T[] a) {
+        throw new UnsupportedOperationException();
+    }
+
+    public boolean add(E e) {
+        throw new UnsupportedOperationException();
+    }
+
+    public boolean remove(Object o) {
+        Iterator<E> e = iterator();
+        if (o==null) {
+            while (e.hasNext()) {
+                if (e.next()==null) {
+                    e.remove();
+                    return true;
+                }
+            }
+        } else {
+            while (e.hasNext()) {
+                if (o.equals(e.next())) {
+                    e.remove();
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public boolean containsAll(Collection<?> c) {
+        Iterator<?> e = c.iterator();
+        while (e.hasNext())
+            if (!contains(e.next()))
+                return false;
+        return true;
+    }
+
+    public boolean addAll(Collection<? extends E> c) {
+        boolean modified = false;
+        Iterator<? extends E> e = c.iterator();
+        while (e.hasNext()) {
+            if (add(e.next()))
+                modified = true;
+        }
+        return modified;
+    }
+
+    public boolean removeAll(Collection<?> c) {
+        boolean modified = false;
+        Iterator<?> e = iterator();
+        while (e.hasNext()) {
+            if (c.contains(e.next())) {
+                e.remove();
+                modified = true;
+            }
+        }
+        return modified;
+    }
+
+    public boolean retainAll(Collection<?> c) {
+        boolean modified = false;
+        Iterator<?> e = iterator();
+        while (e.hasNext()) {
+            if (!c.contains(e.next())) {
+                e.remove();
+                modified = true;
+            }
+        }
+        return modified;
+    }
+}
