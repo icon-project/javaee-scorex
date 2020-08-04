a238891ad5b59aa5fe6cd394f798fe2d06a923d7
Jaechang Namgoong <jnamgoong@iconloop.com>
2020-08-04 19:02:17 +0900

    Add StringTokenizer

---
+++ scorex/src/main/java/scorex/util/StringTokenizer.java
@@ -0,0 +1,66 @@
+package scorex.util;
+
+import java.util.NoSuchElementException;
+
+/**
+ * A simplified version of the java.util.StringTokenizer
+ *
+ * Neither support surrogates nor return delimiters as a token.
+ */
+public class StringTokenizer {
+    private int currentPosition;
+    private int newPosition;
+    private final int maxPosition;
+    private final String str;
+    private final String delimiters;
+
+    public StringTokenizer(String str, String delim) {
+        currentPosition = 0;
+        newPosition = -1;
+        this.str = str;
+        maxPosition = str.length();
+        delimiters = delim;
+    }
+
+    public boolean hasMoreTokens() {
+        newPosition = skipDelimiters(currentPosition);
+        return (newPosition < maxPosition);
+    }
+
+    public String nextToken() {
+        currentPosition = (newPosition >= 0) ?
+                newPosition : skipDelimiters(currentPosition);
+        newPosition = -1;
+
+        if (currentPosition >= maxPosition)
+            throw new NoSuchElementException();
+        int start = currentPosition;
+        currentPosition = scanToken(currentPosition);
+        return str.substring(start, currentPosition);
+    }
+
+    private int skipDelimiters(int startPos) {
+        if (delimiters == null) {
+            throw new NullPointerException();
+        }
+        int position = startPos;
+        while (position < maxPosition) {
+            char c = str.charAt(position);
+            if (delimiters.indexOf(c) < 0)
+                break;
+            position++;
+        }
+        return position;
+    }
+
+    private int scanToken(int startPos) {
+        int position = startPos;
+        while (position < maxPosition) {
+            char c = str.charAt(position);
+            if (delimiters.indexOf(c) >= 0)
+                break;
+            position++;
+        }
+        return position;
+    }
+}
---
+++ scorex/src/test/java/scorex/util/StringTokenizerTest.java
@@ -0,0 +1,29 @@
+package scorex.util;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class StringTokenizerTest {
+    @Test
+    void hasMoreTokens() {
+        assertFalse(new StringTokenizer("", ",").hasMoreTokens());
+        assertFalse(new StringTokenizer(",", ",").hasMoreTokens());
+        assertTrue(new StringTokenizer("a,", ",").hasMoreTokens());
+        assertTrue(new StringTokenizer("a,b", ",").hasMoreTokens());
+        assertTrue(new StringTokenizer(",b", ",").hasMoreTokens());
+    }
+
+    @Test
+    void nextToken() {
+        String input = "a,b,c,d,e,";
+        StringTokenizer tokenizer = new StringTokenizer(input, ",");
+        StringBuilder builder = new StringBuilder();
+        while (tokenizer.hasMoreTokens()) {
+            builder.append(tokenizer.nextToken()).append(",");
+        }
+        assertEquals(input, builder.toString());
+    }
+}
